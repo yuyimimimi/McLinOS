94dce4f8d3cc7e73344524de02553b4c65686214
Siriussann <3174096586@qq.com>
2026-03-01 22:53:04 +0800

    bootloader update

---
+++ bootloader/boot_stm32h743/build/load.d
@@ -0,0 +1,15 @@
+build/load.o: loader/load.c \
+ Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h743xx.h \
+ Drivers/CMSIS/Include/core_cm7.h Drivers/CMSIS/Include/cmsis_version.h \
+ Drivers/CMSIS/Include/cmsis_compiler.h Drivers/CMSIS/Include/cmsis_gcc.h \
+ Drivers/CMSIS/Include/mpu_armv7.h \
+ Drivers/CMSIS/Device/ST/STM32H7xx/Include/system_stm32h7xx.h \
+ loader/../../../../../include/generated/autoconf.h
+Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h743xx.h:
+Drivers/CMSIS/Include/core_cm7.h:
+Drivers/CMSIS/Include/cmsis_version.h:
+Drivers/CMSIS/Include/cmsis_compiler.h:
+Drivers/CMSIS/Include/cmsis_gcc.h:
+Drivers/CMSIS/Include/mpu_armv7.h:
+Drivers/CMSIS/Device/ST/STM32H7xx/Include/system_stm32h7xx.h:
+loader/../../../../../include/generated/autoconf.h:
